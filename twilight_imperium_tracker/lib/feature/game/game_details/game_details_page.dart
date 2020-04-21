f8067749ddb9cf75aba4f914c92bae8d8f17d678
Daniel <danielojewski@gmail.com>
2020-04-21 19:53:50 +0200

    feat: working on actual game details

--- twilight_imperium_tracker/lib/feature/game/game_details/game_details_page.dart
+++ twilight_imperium_tracker/lib/feature/game/game_details/game_details_page.dart
@@ -41,41 +41,14 @@ class GameDetails extends StatelessWidget {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Text(
-                          "${translations.text('my_race')}"
-                          "${getUserFriendlyRaceName(translations, game.raceUsed)}",
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Text(
-                          "${translations.text('collected')} "
-                          "${game.points} ${translations.text('points')} "
-                          "${translations.text('goal_was')} ${game.goal}",
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      ListView.builder(
-                          shrinkWrap: true,
-                          physics: ClampingScrollPhysics(),
-                          itemCount: game.opponents.length + 1,
-                          itemBuilder: (context, index) {
-                            if (index == 0)
-                              return Padding(
-                                padding: const EdgeInsets.all(2.0),
-                                child: Text("${translations.text('opponents')}"),
-                              );
-                            return Padding(
-                              padding: const EdgeInsets.only(left: 12.0),
-                              child: Text("* ${getUserFriendlyRaceName(translations, game.opponents[index - 1])}"),
-                            );
-                          })
-                    ],
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: _buildResultIcon(context: context, result: game.result)
+                      ..addAll([
+                        _buildRace(context: context, translations: translations, raceUsed: game.raceUsed),
+                        _buildCollectedPoints(
+                            translations: translations, collectedPoints: game.points, goal: game.goal),
+                        _buildOpponents(translations: translations, opponents: game.opponents),
+                      ]),
                   ),
                 );
               }
@@ -92,15 +65,70 @@ class GameDetails extends StatelessWidget {
     );
   }
 
-  Color _cardColor(GameResult result) {
+  List<Widget> _buildResultIcon({@required BuildContext context, @required GameResult result}) {
+    return [
+      Icon(
+        getResultIcon(result),
+        color: getResultIconColor(result),
+        size: 128,
+      ),
+      Text(
+        _resultRationale(result: result),
+        style: Theme.of(context).textTheme.bodyText1,
+      )
+    ];
+  }
+
+  String _resultRationale({@required GameResult result}) {
     switch (result) {
       case GameResult.WIN:
-        return Colors.lightGreenAccent;
-      case GameResult.LOSE:
-        return Colors.redAccent;
+        return "Congratulation! You have won.";
       case GameResult.DRAW:
-        return Colors.grey;
+        return "Not bad. It's a draw.";
+      case GameResult.LOSE:
+        return "Better luck next time.";
     }
-    return Colors.white;
+  }
+
+  Widget _buildRace({@required BuildContext context, @required Translations translations, @required Race raceUsed}) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Text(
+        "${translations.text('my_race')}"
+        "${getUserFriendlyRaceName(translations, raceUsed)}",
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget _buildCollectedPoints(
+      {@required Translations translations, @required int collectedPoints, @required int goal}) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Text(
+        "${translations.text('collected')} "
+        "$collectedPoints ${translations.text('points')} "
+        "${translations.text('goal_was')} $goal",
+        textAlign: TextAlign.left,
+      ),
+    );
+  }
+
+  Widget _buildOpponents({@required Translations translations, @required List<Race> opponents}) {
+    return ListView.builder(
+        shrinkWrap: true,
+        physics: ClampingScrollPhysics(),
+        itemCount: opponents.length + 1,
+        itemBuilder: (context, index) {
+          if (index == 0)
+            return Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Text("${translations.text('opponents')}"),
+            );
+          return Padding(
+            padding: const EdgeInsets.only(left: 12.0),
+            child: Text("* ${getUserFriendlyRaceName(translations, opponents[index - 1])}"),
+          );
+        });
   }
 }
